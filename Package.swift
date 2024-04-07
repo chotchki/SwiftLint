3bdbc08868cfeeb832a5a2f6fe3f97bb164243da
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2024-04-07 15:55:09 -0700

    Update Package.swift - to repoint at the sourcekitten with MacCatalyst fixed

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.2.1")),
         .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.0"),
         .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.34.1")),
+        .package(url: "https://github.com/chotchki/SourceKitten.git", branch: "catalyst-fix"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
